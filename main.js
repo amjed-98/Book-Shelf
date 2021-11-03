2cb3ac362fba2bec47592f432b7026f700410b55
Amjad Yahia Roben Hassan <amjedyehia1998@gmail.com>
2021-11-03 23:12:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 // Importing library
-import { select, create } from "./MyLibrary.js";
+import { create, log, select, is_url } from "./MyLibrary.js";
 // --------------------------------------------------------------- //
 
-/* start
- **************************************** */
-let main = select("main");
-let bookContainer = create("div", "bookContainer");
+const bookContainer = select(".book-shelf");
 
 let books = [
   {
@@ -60,75 +57,109 @@ let books = [
   },
 ];
 
-/* rendering books
- **************************************** */
-for (let b of books) {
-  let book = create("div", `book`);
-  let img = create("img");
-  let title = create("span", "title");
-  let author = create("span", "author");
-
-  title.append(b.title);
-  author.append(b.author);
-  img.setAttribute("src", b.image);
-
-  book.append(img, title, author);
-  bookContainer.append(book);
-  main.append(bookContainer);
-}
-
-/* adding new books
- **************************************** */
-const window = select(".window");
-const author = select("#author");
-const title = select("#title");
-const link = select("#link");
-const btn = select("button");
-const addBtn = select('input[value="Add Book"]');
-const close = select(".close");
-const error = select(".error");
-
-function showWindow() {
-  window.classList.add("active");
-  bookContainer.classList.add("active");
-}
-
-function closeWindow() {
-  window.classList.remove("active");
-  bookContainer.classList.remove("active");
-}
-
-// adding new book function
-function addBook(e) {
-  e.preventDefault();
-  if (title.value === "" || author.value === "" || link.value === "") {
-    error.style.display = "block";
-  } else {
-    let newBook = {
-      title: title.value,
-      author: author.value,
-      image: link.value,
-    };
-
-    books.push(newBook); // pushing the new book info into the array
-
-    for (let i = 0; i < 1; i++) {
-      let book = create("div", `book`);
-      let img = create("img");
-      let title = create("span", "title");
-      let author = create("span", "author");
-
-      img.setAttribute("src", newBook.image);
-      title.append(newBook.title);
-      author.append(newBook.author);
-
-      book.append(img, title, author);
-      bookContainer.append(book);
-      main.append(bookContainer);
-    }
+class Book {
+  constructor(title, author, image) {
+    this.title = title;
+    this.author = author;
+    this.image = image;
   }
 }
 
-btn.addEventListener("click", showWindow);
-close.addEventListener("click", closeWindow);
-addBtn.addEventListener("click", addBook);
+// rendering books to the DOM
+for (let i = 0; i < books.length; i++) {
+  const book = new Book(books[i].title, books[i].author, books[i].image);
+
+  let bookEl = create("div", `book`);
+  let titleEl = create("span", "title");
+  let authorEl = create("span", "author");
+  let imgEl = create("img");
+
+  titleEl.append(book["title"]);
+  authorEl.append(book["author"]);
+  imgEl.append(book["image"]);
+  imgEl.setAttribute("src", books[i]["image"]);
+
+  bookEl.append(imgEl, titleEl, authorEl);
+  bookContainer.append(bookEl);
+  document.body.append(bookContainer);
+}
+
+// -------------------------------------------------------------------------------------------------- //
+
+// add window popup Section
+const errorMsg = select(".error");
+const successMsg = select(".success");
+const uploadBtn = select("button");
+const form = select("form");
+const titleInput = select('input[name="title"]');
+const authorInput = select('input[name="author"]');
+const urlInput = select('input[name="url"]');
+const addButton = select('input[type="submit"]');
+
+// preventing form from submitting
+form.onsubmit = (e) => e.preventDefault();
+
+// toggle form
+uploadBtn.onclick = () => form.classList.toggle("active");
+
+// close form window
+document.onkeydown = (e) => {
+  if (e.key === "Escape") form.classList.remove("active");
+};
+
+// display success Msg
+const success = () => (successMsg.style.display = "block");
+// display error Msg
+const error = () => (errorMsg.style.display = "block");
+
+// render new book to the DOM
+addButton.onclick = () => {
+  if (
+    titleInput.value !== "" &&
+    authorInput.value !== "" &&
+    is_url(urlInput.value)
+  ) {
+    // grab inputField values
+    let titleText = titleInput.value;
+    let authorText = authorInput.value;
+    let urlText = urlInput.value;
+
+    // push the new book to the books Array
+    books.push({
+      title: titleText,
+      author: authorText,
+      image: urlText,
+    });
+
+    // create components
+    let bookEl = create("div", `book`);
+    let titleEl = create("span", "title");
+    let authorEl = create("span", "author");
+    let imgEl = create("img");
+
+    // grab new book
+    const newBook = books[books.length - 1];
+
+    //  append newBook properties to components
+    titleEl.append(newBook.title);
+    authorEl.append(newBook.author);
+    imgEl.append(newBook.image);
+    imgEl.setAttribute("src", newBook.image);
+
+    // appending components
+    bookEl.append(imgEl, titleEl, authorEl);
+    bookContainer.append(bookEl);
+    document.body.append(bookContainer);
+
+    success();
+    errorMsg.style.display = "none";
+  } else {
+    error();
+    successMsg.style.display = "none";
+  }
+
+  // clearing InputFields
+  titleInput.value = "";
+  authorInput.value = "";
+  urlInput.value = "";
+};
